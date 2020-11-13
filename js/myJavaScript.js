adf97b44b3fe9cb9692d3d7200610d6b684bf432
Michellexinhui0 <71323755+Michellexinhui0@users.noreply.github.com>
2020-11-13 16:36:41 +0800

    Add files via upload

---
+++ js/myJavaScript.js
@@ -0,0 +1,44 @@
+
+function Submit(){
+      // Find the element
+   		x=document.getElementById("demo");
+       //Option 1: Change the style attribute directly
+		x.style.display="block";
+    alert("Thank you for showing your interest to know me better! I will get in touch with you real soon!");
+      //Option 2: Change the class
+  x.className="opened";
+	}
+
+  function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.innerHTML === "Welcome to my E-Resume!") {
+      x.innerHTML = "Glad that you are here :)";
+    } 
+    else if (x.innerHTML ===  "Glad that you are here :)") {
+      x.innerHTML = "Thank you for taking out your precious time to take a look at my E-Resume. Appreciate it a lot!";
+    } 
+    else {
+      x.innerHTML = "Let's start looking through my E-Resume!";
+    }
+  }
+
+  var i = 0;
+  var txt = 'Hello I am Michelle Ling Xin Hui. I am 19 years old. I am currently pursuing a Diploma in Information Technology at Taylors University. I am looking for opportunities in UI/UX design, Website Development, Information System Management, Digital Marketing, and Software Management.';
+  var speed = 50;
+  
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("demo").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+  
+
+  
+
+
+
+
+  
